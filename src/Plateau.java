bf9aa709bb5d8bf70409083c1050b426ed8f3b84
Lorhaki <flament.lolo@laposte.net>
2022-11-24 08:32:14 +0100

    Initial commit

---
+++ src/Plateau.java
@@ -0,0 +1,248 @@
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+public class Plateau {
+	//Tableau d'entier pour représenter les cases d'un tableau
+	private int [][] cases;
+	//i et j pour coordonnées d'une case
+	private int i , j ;
+	private int nbrBlanc , nbrNoir , nbrVide ;
+	//Liste des coups possibles pour les pions noir et blanc
+	private ArrayList<CoupPossible> ListeBlanc;
+	private ArrayList<CoupPossible> ListeNoir;
+	
+	//Constructeur pour initailaiser un plateau classique d'Othello
+	//On considére que le 1 c'est pour les blancs et 2 pour les noirs
+	public Plateau(){
+		this.i = 8;
+		this.j = 8;
+		this.cases = new int [8][8];
+		for(int i = 0 ; i < this.i; i++) {
+			for(int j = 0 ; j< this.j ; j++) {
+				this.cases[i][j] = 0 ;
+			}
+		}
+	//pour mettre les pions qui sont posés de base sur le palteau.
+	cases[3][3] = 1 ; 
+	cases[4][4] = 1 ; 
+	cases[4][3] = 2 ; 
+	cases[3][4] = 2 ; 
+	this.NbrBlanc();
+	this.NbrNoir();
+	this.NbrVide();
+	this.ListeBlanc = new ArrayList<CoupPossible>();
+	this.ListeNoir = new ArrayList<CoupPossible>();
+	
+	}
+	
+
+	//méthode pour afficher le plateau avec les coordonnées des cases sur les cotés de celui-ci
+	public void AfficherPlateau() {
+		System.out.print(" ");
+		for(int A = 1 ; A < 9 ; A++) {
+			System.out.print(" ");
+			System.out.print(A);
+			}
+		System.out.println();
+		for(int i = 0 ; i < this.i; i++) {
+			System.out.print(i+1);
+			for(int j = 0 ; j< this.j ; j++) {
+				System.out.print(" ");
+				System.out.print(cases[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	//Permet de calculer le nombre de pion blanc sur le plateau
+	public void NbrBlanc(){
+		int nbrBlanc = 0 ;
+		for(int i = 0 ; i < this.i; i++) {
+			for(int j = 0 ; j< this.j ; j++) {
+				if(this.cases[i][j] == 1)
+					nbrBlanc++;
+			}		
+		}
+		this.nbrBlanc = nbrBlanc ; 
+	}
+	
+	//Permet de calculer le nombre de pion noir sur le plateau
+	public void NbrNoir(){
+		int nbrNoir = 0 ;
+		for(int i = 0 ; i < this.i; i++) {
+			for(int j = 0 ; j< this.j ; j++) {
+				if(this.cases[i][j] == 2)
+					nbrNoir++;
+			}		
+		}
+		this.nbrNoir=nbrNoir; 
+	}
+	
+	//Permet de calculer le nombre de cases vides du plateau
+	public void NbrVide(){
+		int nbrVide = 0 ;
+		for(int i = 0 ; i < this.i; i++) {
+			for(int j = 0 ; j< this.j ; j++) {
+				if(this.cases[i][j] == 0)
+					nbrVide++;
+			}		
+		}
+		this.nbrVide = nbrVide ;
+	}
+	
+	//Permet de calculer le nombre de coup possible pour les pions noir
+	public int nbrCoupPossibleNoir() {
+		int NbrPossible = 0;
+		return NbrPossible;
+	}
+	
+	//Permet de calculer le nombre de coup possible pour le joueur noir
+	public int nbrCoupPossibleBlanc() {
+		int NbrPossible = 0;
+		return NbrPossible;
+	}
+	
+	public boolean coupPossibleNoir(int i , int j){
+		boolean a = false;
+		
+		return a ; 
+	}
+	public boolean coupPossibleBlanc(int i , int j){
+		boolean a = false;
+		
+		return a ; 
+	}
+	public void mettreAjourLePlateau() {
+		
+	}
+	public void JouerUnCoup(int i , int j) {
+		
+	}
+	
+	
+	public boolean regardeAutourNoir(int i , int j) {
+		if(this.cases [i+1][j+1] == 1) {
+			return true ;
+		}
+		if(this.cases [i+1][j] == 1) {
+			return true ;
+		}
+		if(this.cases [i][j+1] == 1) {
+			return true ;
+		}
+		if(this.cases [i-1][j-1] == 1) {
+			return true ;
+		}
+		if(this.cases [i-1][j] == 1) {
+			return true ;
+		}
+		if(this.cases [i][j-1] == 1) {
+			return true ;
+		}
+		if(this.cases [i+1][j-1] == 1) {
+			return true ;
+		}
+		if(this.cases [i-1][j+1] == 1) {
+			return true ;
+		}
+		return false;
+	}
+	
+public boolean regardeAutourBlanc(int i , int j) {
+		
+	if(this.cases [i+1][j+1] == 2) {
+		return true ;
+	}
+	if(this.cases [i+1][j] == 2) {
+		return true ;
+	}
+	if(this.cases [i][j+1] == 2) {
+		return true ;
+	}
+	if(this.cases [i-1][j-1] == 2) {
+		return true ;
+	}
+	if(this.cases [i-1][j] == 2) {
+		return true ;
+	}
+	if(this.cases [i][j-1] == 2) {
+		return true ;
+	}
+	if(this.cases [i+1][j-1] == 2) {
+		return true ;
+	}
+	if(this.cases [i-1][j+1] == 2) {
+		return true ;
+	}
+	return false;
+}
+	//GET ET SET
+	
+	
+	public int[][] getCases() {
+		return cases;
+	}
+
+	public void setCases(int[][] cases) {
+		this.cases = cases;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	public int getNbrBlanc() {
+		return nbrBlanc;
+	}
+
+	public void setNbrBlanc(int nbrBlanc) {
+		this.nbrBlanc = nbrBlanc;
+	}
+
+	public int getNbrNoir() {
+		return nbrNoir;
+	}
+
+	public void setNbrNoir(int nbrNoir) {
+		this.nbrNoir = nbrNoir;
+	}
+
+	public int getNbrVide() {
+		return nbrVide;
+	}
+
+	public void setNbrVide(int nbrVide) {
+		this.nbrVide = nbrVide;
+	}
+	public ArrayList<CoupPossible> getListeBlanc() {
+		return ListeBlanc;
+	}
+
+
+	public void setListeBlanc(ArrayList<CoupPossible> listeBlanc) {
+		ListeBlanc = listeBlanc;
+	}
+
+
+	public ArrayList<CoupPossible> getListeNoir() {
+		return ListeNoir;
+	}
+
+
+	public void setListeNoir(ArrayList<CoupPossible> listeNoir) {
+		ListeNoir = listeNoir;
+	}
+}
---
+++ src/TestPlateau.java
@@ -0,0 +1,29 @@
+public class TestPlateau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//On initialise ici notre plateau de jeu 
+		Plateau Othello = new Plateau();
+		//On affiche le plateau de jeu
+		Othello.AfficherPlateau();
+		System.out.println("Voici le nombre de cases vide : "+ Othello.getNbrVide());
+		System.out.println("Voici le nombre de cases Blanche : "+ Othello.getNbrBlanc());
+		System.out.println("Voici le nombre de cases Noires : "+ Othello.getNbrNoir());
+		
+		//On commence la partie
+		int a = 1 ;
+		int b = 1 ;
+		while(Othello.getNbrBlanc() != 0 || Othello.getNbrNoir() != 0 ||Othello.getNbrVide() != 0 || (a != 0 && b!=0)) {
+			//au joueur 1 de joueur
+				System.out.println("c'est au joueur 1 de jouer");
+				
+				System.out.println("c'est au joueur 2 de jouer");
+				
+			}
+		}
+	
+		
+		
+	}
+
+
